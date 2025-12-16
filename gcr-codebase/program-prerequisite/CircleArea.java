02c08d49c5f7b7a0932654ce5f96ce3cd419f950
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2025-12-16 23:35:42 +0530

    [Sarthak]: added Circle area calculation program

---
+++ gcr-codebase/program-prerequisite/CircleArea.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CircleArea{
+
+public class main( String[] args){
+
+Scanner sc =new Scanner(System.in);
+
+int radius =sc.nextFloat();
+int r2 =  radius * radius;
+
+float Area = 3.14 * r2;
+
+System.out.println("Fahrenheit : "+ Fahrenheit );
+
+}
+
+}
