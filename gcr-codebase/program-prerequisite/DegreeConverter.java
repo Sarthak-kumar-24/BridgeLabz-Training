bc0002102c1997b3d2aa719e759745ea540fb5ca
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2025-12-16 23:34:43 +0530

    [Sarthak]: added Temperature convertor program

---
+++ gcr-codebase/program-prerequisite/DegreeConverter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DegreeConverter{
+
+public class main( String[] args){
+
+Scanner sc =new Scanner(System.in);
+
+float Celsius =sc.nextFloat();
+
+float Fahrenheit = ( Celsius * 9/5) + 32;
+
+System.out.println("Fahrenheit : "+ Fahrenheit );
+
+}
+
+}
