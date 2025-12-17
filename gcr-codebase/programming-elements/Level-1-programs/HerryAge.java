3c6471d1b578258cca32be15090f24a3d06ffbd1
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2025-12-17 16:11:15 +0530

    [Sarthak]: added herry Age finder program

--- gcr-codebase/programming-elements/Level-1-programs/HerryAge.java
+++ gcr-codebase/programming-elements/Level-1-programs/HerryAge.java
@@ -1,14 +1,18 @@
+//creating a class herryAge to calculate the current age of herry
 
-class HerryAge{
+
+public class HerryAge{
 
 public static void main(String[] args){
 
      
                        int HerryBirthYear = 2000;
+
+
                        int CurrentAge = 2024 - 2000;
 
 
-                     System.out.println(" Harry's age in 2024 is" + CurrentAge );
+                       System.out.println(" Harry's age in 2024 is" + CurrentAge );
               }
 
 
