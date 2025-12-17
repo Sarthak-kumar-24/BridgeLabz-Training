f6910dfadd3b1f3c26a73f25835f6a10a8daea8c
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2025-12-17 11:54:00 +0530

    [Sarthak]: added herry's Age calculation program

---
+++ gcr-codebase/programming-elements/Level-1-programs/HerryAge.java
@@ -0,0 +1,15 @@
+
+class HerryAge{
+
+public static void main(String[] args){
+
+     
+                       int HerryBirthYear = 2000;
+                       int CurrentAge = 2024 - 2000;
+
+
+                     System.out.println(" Harry's age in 2024 is" + CurrentAge );
+              }
+
+
+}
