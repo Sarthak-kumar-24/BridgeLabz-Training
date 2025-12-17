2fdde298eb16195011283e5a1626b4bbb723da23
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2025-12-17 15:34:22 +0530

    [Sarthak]: added Square side from permiter calculation program

---
+++ gcr-codebase/programming-elements/Level-1-programs/SquareSideFromPerimeter.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+//creating a class named SquareSideFromPerimeter to find length of side of square by its perimeter
+
+
+
+
+public class SquareSideFromPerimeter {
+
+    public static void main(String[] args) {
+
+         Scanner input = new Scanner(System.in);
+
+        // Taking perimeter from user
+        double perimeter = input.nextDouble();
+
+        // Perimeter of square = 4 * side
+        double side = perimeter / 4;
+
+        // Displaying thr  result
+        System.out.println(
+                "The length of the side is " + side +
+                " whose perimeter is " + perimeter
+        );
+
+        // Close scanner
+        input.close();
+    }
+}
