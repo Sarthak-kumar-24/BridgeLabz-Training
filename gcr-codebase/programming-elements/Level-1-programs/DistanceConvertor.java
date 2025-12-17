04024669a8bd33496e0092b29b2439bedf20210b
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2025-12-17 14:19:13 +0530

    [Sarthak]: added Miles to Km conversion program

---
+++ gcr-codebase/programming-elements/Level-1-programs/DistanceConvertor.java
@@ -0,0 +1,13 @@
+class DistanceConvertor{
+
+      public static void main(String[] args){
+
+             float distanceInKm = 10.8;
+
+             // Conversion of Km to Miles by multiplying by 1.6
+             float distanceInMiles = distanceInKm * 1.6;
+
+             //Final output  in Miles
+             System.out.println( " The distance "+  distanceInKm + " km in miles is "+  distanceInMiles );
+      }
+}
