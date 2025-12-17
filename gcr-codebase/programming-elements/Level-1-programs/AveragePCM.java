5e1c498026ed179b80328070d838b095619e7934
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2025-12-17 14:11:23 +0530

    [Sarthak]: added Average PCM programprograms/AveragePCM.java

---
+++ gcr-codebase/programming-elements/Level-1-programs/AveragePCM.java
@@ -0,0 +1,12 @@
+public class AveragePCM {
+    public static void main(String[] args) {
+
+        int marksInMaths = 94;
+        int marksInPhysics = 95;
+        int marksInChemistry = 96;
+
+        double average = (marksInMaths + marksInPhysics + marksInChemistry) / 3.0;
+
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }
+}
