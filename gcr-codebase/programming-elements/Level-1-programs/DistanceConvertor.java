ee0f5b905b0a39c946f35ff3d8688397d297b59f
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2025-12-17 16:10:21 +0530

    [Sarthak]: added DistanceConvertor program

--- gcr-codebase/programming-elements/Level-1-programs/DistanceConvertor.java
+++ gcr-codebase/programming-elements/Level-1-programs/DistanceConvertor.java
@@ -1,11 +1,13 @@
+//creating a class named DistanceConvertor to convert distance in km to miles
 class DistanceConvertor{
-
       public static void main(String[] args){
 
              float distanceInKm = 10.8;
 
+
              // Conversion of Km to Miles by multiplying by 1.6
-             float distanceInMiles = distanceInKm * 1.6;
+             float distanceInMiles = distanceInKm / 1.6;
+
 
              //Final output  in Miles
              System.out.println( " The distance "+  distanceInKm + " km in miles is "+  distanceInMiles );
