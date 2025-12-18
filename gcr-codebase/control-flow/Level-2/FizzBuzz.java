ae4872aa8ea6215df99fa76e4d854edfb49d1191
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2025-12-18 23:27:41 +0530

    [Sarthak]: added FizzBuzz program

---
+++ gcr-codebase/control-flow/Level-2/FizzBuzz.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+
+
+//creating a class named FizzBuzz   
+
+class FizzBuzz{
+      public static void main(String[] args){
+           Scanner sc = new Scanner(System.in);
+
+          // taking the number as input
+           int  number = sc.nextInt();
+
+
+
+           if( number > 0){
+
+                  for (int i = 1; i <= number; i++) {
+                    
+                   // checking if it is divisible by 3 and 5 both
+                   if(int i %  3 == 0 && i % 5 == 0){
+                           System.out.println(" FizzBuzz");
+                    } 
+                    
+                    // checking if it is divisible by 3
+                    else if(int  i %  3 == 0){
+                           System.out.println(" Fizz");
+                    }
+
+                     // checking if it is divisible by 5
+                     else  if( i % 5 == 0){
+
+                           System.out.println("Buzz ");
+
+                    }else{
+
+                            System.out.println(i);
+                   }
+
+
+           } else {
+
+                   System.out.println("Please enter a positive integer");
+           }
+           
+
+      
+          sc.close();
+
+
+     }
+}
