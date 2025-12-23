35b8ca8b040c7d5997f2ee1fea550d4d5fd66811
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2025-12-23 21:03:47 +0530

    [Sarthak]: added NumberFormatExceptionClass program

---
+++ gcr-codebase/Strings/level-1/NumberFormatExceptionClass.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+
+/*
+ Created a class named NumberFormatExceptionClass that
+demonstrates NumberFormatException using String Integer.parseInt(). and shows 
+Trying to convert a non-numeric String into an integer using Integer.parseInt()
+ */
+public class  NumberFormatExceptionClass{
+
+
+   //generating the exception
+   public static void generateException( String text){
+
+       System.out.println( Integer.parseInt(text));
+
+   }
+
+   //handling the exception
+   public static void  handleException(String text){
+        try {
+            
+            System.out.println(Integer.parseInt(text));
+
+        } catch (NumberFormatException e) {
+
+            System.out.println("NumberFormatException caught and handled.");
+        } catch (RuntimeException e) {
+
+            System.out.println(" generic RuntimeException caught.");
+        }
+   }
+
+   public static void main( String[] args){
+
+       Scanner sc = new Scanner(System.in);
+
+       //getting user input
+       String text = sc.next();
+
+       try{
+
+          generateException( text );
+       } catch (NumberFormatException e){
+
+       System.out.println( "Exception occurred in generateException().");
+       }
+
+
+       handleException(text);
+       sc.close();
+
+   }
+}
+
