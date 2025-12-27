c337352acb0518fd09719eca63e8b6e6686c0a54
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2025-12-27 21:35:45 +0530

    [Sarthak]: added three sum closest leetcode program

---
+++ leet-code-codebase/arrays/ThreeSumClosest.java
@@ -0,0 +1,55 @@
+/*
+
+problem : 3 sum closest
+
+Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+
+Return the sum of the three integers.
+
+You may assume that each input would have exactly one solution.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+Example 2:
+
+Input: nums = [0,0,0], target = 1
+Output: 0
+Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+
+*/
+
+
+class ThreeSumClosest {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        int closest_sum = Integer.MAX_VALUE / 2;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int current_sum = nums[i] + nums[left] + nums[right];
+                if (Math.abs(current_sum - target) < Math.abs(closest_sum - target)) {
+                    closest_sum = current_sum;
+                }
+                if (current_sum < target) {
+                    ++left;
+                } else if (current_sum > target) {
+                    --right;
+                } else {
+                    return current_sum;
+                }
+
+            }
+        }
+        return closest_sum;
+    }
+}
