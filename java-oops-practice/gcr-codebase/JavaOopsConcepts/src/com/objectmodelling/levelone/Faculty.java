ffb629c53d7e306d10a04251310ca3b301fea128
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2025-12-30 22:52:53 +0530

    [Sarthak]: added faculty program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/levelone/Faculty.java
@@ -0,0 +1,26 @@
+package com.objectmodelling.levelone;
+
+/*
+ * Description: Create a University with multiple Faculty members and Department objects. 
+ * Model it so that the University and its Departments are in a composition relationship 
+ * (deleting a university deletes all departments), 
+ * and the Faculty members are in an aggregation relationship (faculty can exist outside of any specific department).
+
+Tasks:
+Define a University class with Department and Faculty classes.
+Demonstrate how deleting a University also deletes its Departments.
+Show that Faculty members can exist independently of a Department.
+ * 
+ */
+public class Faculty {
+
+    private String name;
+
+    Faculty(String name) {
+        this.name = name;
+    }
+
+    void showFaculty() {
+        System.out.println(" Faculty: " + name);
+    }
+}
