7ffbc18493e16a1916dd4d4849b9ec6c5b08e4e8
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2025-12-31 22:16:40 +0530

    [Sarthak]: added Animal hierarchy program's dog class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalinheritance/animalhierarchy/Dog.java
@@ -0,0 +1,26 @@
+package com.inheritance.hierarchicalinheritance.animalhierarchy;
+
+/*
+ * Animal Hierarchy
+ * 
+ * Description: Create a hierarchy where Animal is the superclass, 
+ * and Dog, Cat, and Bird are subclasses. 
+ * Each subclass has a unique behavior.
+ * 
+ * 
+ * Define a superclass Animal with attributes name and age, and a method makeSound().
+ * Define subclasses Dog, Cat, and Bird, 
+ * each with a unique implementation of makeSound().
+ */
+public class Dog extends Animal{
+
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Dog barks ");
+    }
+
+}
