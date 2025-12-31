9bf6e666c51eab23da957c28d2c23bf8bccde778
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2025-12-31 22:36:59 +0530

    [Sarthak]: added Vehicle-Management-System program's Refuelable class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/vehiclemanagementsystem/Refuelable.java
@@ -0,0 +1,18 @@
+package com.inheritance.hybridinheritance.vehiclemanagementsystem;
+
+/*Vehicle Management System
+ * 
+ * Description: Model a vehicle system where Vehicle is the superclass and ElectricVehicle and PetrolVehicle are subclasses. 
+ * Additionally, create a Refuelable interface implemented by PetrolVehicle
+ * 
+ * 
+ * Define a superclass Vehicle with attributes like maxSpeed and model.
+ * Create an interface Refuelable with a method refuel().
+ * Define subclasses ElectricVehicle and PetrolVehicle. 
+ * PetrolVehicle should implement Refuelable, while ElectricVehicle include a charge() method.
+
+ */
+public interface Refuelable {
+
+	void refuel();
+}
