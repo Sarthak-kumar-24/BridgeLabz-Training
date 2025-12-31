fd118e91a1d8b46175c859fee84d79731ccf4306
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2025-12-31 22:33:28 +0530

    [Sarthak]: added Restaurant-Management-System program's Waiter class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/restaurantmanagementsystem/Waiter.java
@@ -0,0 +1,24 @@
+package com.inheritance.hybridinheritance.restaurantmanagementsystem;
+
+/*Restaurant Management System
+ * 
+ * Description: Model a restaurant system where Person is the superclass and Chef and Waiter are subclasses. 
+ * Both Chef and Waiter should implement a Worker interface that requires a performDuties() method
+ * 
+ * 
+ * Create an interface Worker with a method performDuties().
+ * Define subclasses Chef and Waiter that inherit from Person and implement the Worker interface, 
+ * each providing a unique implementation of performDuties().
+
+ */
+public class Waiter extends Person implements Worker {
+
+    public Waiter(String name, int id) {
+        super(name, id);
+    }
+
+    @Override
+    public void performDuties() {
+        System.out.println("Waiter is serving food to customers ");
+    }
+}
