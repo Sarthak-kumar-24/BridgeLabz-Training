bc49d0ecb927f38665beb3828108a2784bc9486a
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-01 22:13:42 +0530

    [Sarthak]: added Banking-System program's Loanable class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/bankingsystem/Loanable.java
@@ -0,0 +1,19 @@
+package com.encapsulationandpolymorphism.bankingsystem;
+
+/* Banking System
+ * 
+ * Description: Create a banking system with different account types:
+ * 
+ * Define an abstract class BankAccount with fields like accountNumber, holderName, and balance.
+ * Add methods like deposit(double amount) and withdraw(double amount) (concrete) and calculateInterest() (abstract).
+ * Implement subclasses SavingsAccount and CurrentAccount with unique interest calculations.
+ * Create an interface Loanable with methods applyForLoan() and calculateLoanEligibility().
+ * Use encapsulation to secure account details and restrict unauthorized access.
+ * Demonstrate polymorphism by processing different account types and calculating interest dynamically.
+
+ */
+public interface Loanable {
+
+    void applyForLoan();
+    double calculateLoanEligibility();
+}
