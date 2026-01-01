5683d924267084383fdb32b4aebcd8f2b723ac62
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-01 22:28:34 +0530

    [Sarthak]: added Library-Management program's Magazine class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/librarymanagementsystem/Magazine.java
@@ -0,0 +1,42 @@
+package com.encapsulationandpolymorphism.librarymanagementsystem;
+
+/* Banking System
+ * 
+ * Description: Create a banking system with different account types:
+ * 
+ * 
+ * Define an abstract class BankAccount with fields like accountNumber, holderName, and balance.
+ * Add methods like deposit(double amount) and withdraw(double amount) (concrete) and calculateInterest() (abstract).
+ * Implement subclasses SavingsAccount and CurrentAccount with unique interest calculations.
+ * Create an interface Loanable with methods applyForLoan() and calculateLoanEligibility().
+ * Use encapsulation to secure account details and restrict unauthorized access.
+ * Demonstrate polymorphism by processing different account types and calculating interest dynamically.
+ * 
+ */
+class Magazine extends LibraryItem implements Reservable {
+
+    public Magazine(int id, String title, String author) {
+        super(id, title, author);
+    }
+
+    @Override
+    public int getLoanDuration() {
+        return 7; // days
+    }
+
+    @Override
+    public void reserveItem(String borrowerName) {
+        if (isAvailable()) {
+            setBorrower(borrowerName);
+            System.out.println("Magazine reserved by " + borrowerName);
+        } else {
+            System.out.println("Magazine is already reserved");
+        }
+    }
+
+    @Override 
+    public boolean checkAvailability() {
+        return isAvailable();
+    }
+}
+
