31dc4cbcd1cfb96f0f4e0fffffa6b40b5829250e
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-01 22:18:12 +0530

    [Sarthak]: added Ecommerce-Platform program's Taxable class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/ecommerceplatform/Taxable.java
@@ -0,0 +1,17 @@
+package com.encapsulationandpolymorphism.ecommerceplatform;
+
+/*E-Commerce Platform
+ * 
+ * Description: Develop a simplified e-commerce platform:
+ * 
+ * Create an abstract class Product with fields like productId, name, and price, and an abstract method calculateDiscount().
+ * Extend it into concrete classes: Electronics, Clothing, and Groceries.
+ * Implement an interface Taxable with methods calculateTax() and getTaxDetails() for applicable product categories.
+ * Use encapsulation to protect product details, allowing updates only through setter methods.
+ * Showcase polymorphism by creating a method that calculates and prints the final price (price + tax - discount) for a list of Product.
+ */
+public interface Taxable {
+	
+	double calculateTax();
+	String getTaxDetails();
+}
