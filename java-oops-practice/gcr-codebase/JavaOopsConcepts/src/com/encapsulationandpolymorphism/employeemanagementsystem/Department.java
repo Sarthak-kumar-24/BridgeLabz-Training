c5cb7772d82a78d488b2fc0b1128f3bfdc3c9675
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-01 22:20:11 +0530

    [Sarthak]: added Employee-Management-System program's Department class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/employeemanagementsystem/Department.java
@@ -0,0 +1,21 @@
+package com.encapsulationandpolymorphism.employeemanagementsystem;
+
+/*
+ * Employee Management System
+ * Description: Build an employee management system with the following requirements:
+ * 
+ * Use an abstract class Employee with fields like employeeId, name, and baseSalary.
+ * Provide an abstract method calculateSalary() and a concrete method displayDetails().
+ * Create two subclasses: FullTimeEmployee and PartTimeEmployee, implementing calculateSalary() based on work hours or fixed salary.
+ * Use encapsulation to restrict direct access to fields and provide getter and setter methods.
+ * Create an interface Department with methods like assignDepartment() and getDepartmentDetails().
+ * Ensure polymorphism by processing a list of employees and displaying their details using the Employee reference.
+ * 
+ */
+
+public interface Department {
+	void assignDepartment(String deptName);
+	String getDepartmentDetails();
+	
+
+}
