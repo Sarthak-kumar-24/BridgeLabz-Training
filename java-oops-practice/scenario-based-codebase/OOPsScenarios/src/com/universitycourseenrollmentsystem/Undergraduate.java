5a1387cd554f2f355ad6034db545d5257d91f02a
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-02 20:41:48 +0530

    [Sarthak]: added University-Course-Enrollment-System program's UnderGraduate class

---
+++ java-oops-practice/scenario-based-codebase/OOPsScenarios/src/com/universitycourseenrollmentsystem/Undergraduate.java
@@ -0,0 +1,25 @@
+package com.universitycourseenrollmentsystem;
+
+/* University Course Enrollment System
+ * 
+ * Scenario: Students should be able to enroll in courses, and faculty can assign grades.
+ * 
+ * Class: Student, Course, Faculty, Enrollment
+ * Constructors: Used to create students with/without elective preferences
+ * Access Modifiers: private for internal GPA fields, public method to access
+ * Interface: Graded with method assignGrade()
+ * Operators: GPA calculations and comparisons (using +, /, etc.)
+ * Inheritance: Undergraduate, Postgraduate extend Student
+ */
+public class Undergraduate extends Student {
+
+    public Undergraduate(int id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Undergraduate Student");
+    }
+}
+
