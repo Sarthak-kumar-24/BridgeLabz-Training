684dff99127cc829091c398b7d8ee4c2b9e4f395
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-02 20:47:21 +0530

    [Sarthak]: added Vehicle-Rental-Application program's Rentable class

---
+++ java-oops-practice/scenario-based-codebase/OOPsScenarios/src/com/vehiclerentalapplication/Rentable.java
@@ -0,0 +1,18 @@
+package com.vehiclerentalapplication;
+
+/* Vehicle Rental Application
+ * 
+ * Scenario: Build a system for renting bikes, cars, and trucks to customers.
+ * 
+ * Class: Vehicle, Bike, Car, Truck, Customer
+ * Constructors: Custom constructors for different vehicle types
+ * Access Modifiers: protected for vehicle attributes to allow subclass access
+ * Interface: Rentable with method calculateRent(int days)
+ * Operators: Used in rent calculation (e.g., baseRate × days ± surcharges)
+ * Inheritance: Car, Bike, Truck inherit from Vehicle
+ * Polymorphism: Rent calculation differs by vehicle type
+ */
+public interface Rentable {
+	double calculateRent(int days);
+
+}
