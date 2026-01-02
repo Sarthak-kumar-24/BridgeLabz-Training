2d209cb654f083e8599a0466431b98133991ba29
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-02 20:34:16 +0530

    [Sarthak]: added Hospital-Management-System program's payable class

---
+++ java-oops-practice/scenario-based-codebase/OOPsScenarios/src/com/hospitalpatientmanagementsystem/Payable.java
@@ -0,0 +1,25 @@
+package com.hospitalpatientmanagementsystem;
+
+/* Hospital Patient Management System
+ * 
+ * Scenario: A hospital wants a software module to register patients, assign doctors, and maintain
+ * billing.
+ * 
+ *  Class: Patient, Doctor, Bill
+ *  Constructor: Overloaded for emergency vs. normal patient admission
+ * Access Modifiers: Keep sensitive data like medicalHistory as private, expose
+ * getSummary() via public
+ * Interface: Payable with method calculatePayment() implemented in Bill
+ * Operators: Used to compute discounts, taxes, totals
+ * OOP:
+ * Encapsulation: Private fields with getter/setters
+ * Abstraction: Hide billing calculation behind interface
+ * Inheritance: InPatient, OutPatient extend Patient
+ * Polymorphism: displayInfo() overridden for Doctor, Patient, etc.
+ * 
+ */
+public interface Payable {
+	
+	double calculatePayment();
+
+}
