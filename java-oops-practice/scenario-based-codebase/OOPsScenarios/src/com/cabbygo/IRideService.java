3d3f98a7bc51b1dc333bda44f7070235fda80a6d
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-03 20:31:21 +0530

    [Sarthak]: added Cab-Bygo program's IRideService class

---
+++ java-oops-practice/scenario-based-codebase/OOPsScenarios/src/com/cabbygo/IRideService.java
@@ -0,0 +1,18 @@
+package com.cabbygo;
+/*
+ * "CabbyGo – A Ride-Hailing App in the Making
+ * 
+ * Story: You’ve joined a startup building CabbyGo, a ride-hailing app. You're tasked with building
+ * the backend logic for managing vehicles, drivers, and ride booking.
+ * 
+ * Vehicle (class): fields like vehicleNumber, capacity, type.
+ * Driver (class): fields like name, licenseNumber, rating.
+ * Constructors to initialize vehicles and drivers.
+ * An IRideService interface with methods like bookRide() and endRide().
+ * Use encapsulation to hide sensitive data like location, fare.
+ * Implement polymorphism with different vehicle types like Mini, Sedan, SUV
+ */
+interface IRideService {
+    void bookRide(double distance);
+    void endRide();
+}
