5982146b9eea244d12b454014852154518de5bb6
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-05 23:19:56 +0530

    [Sarthak]: added Swift-Cart program's Product class

---
+++ java-oops-practice/scenario-based-codebase/OOPsScenarios/src/com/swiftcart/Product.java
@@ -0,0 +1,25 @@
+package com.swiftcart;
+
+public abstract class Product {
+
+    protected String name;
+    protected double price;
+    protected String category;
+
+    public Product(String name, double price, String category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public abstract double getDiscount(); // polymorphism
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
