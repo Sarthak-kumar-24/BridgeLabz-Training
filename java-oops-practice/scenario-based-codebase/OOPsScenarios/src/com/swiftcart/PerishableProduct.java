96975860f6239e33ce64f51633da906cf12b93c7
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-05 23:19:28 +0530

    [Sarthak]: added Swift-Cart program's PerishableProduct class

---
+++ java-oops-practice/scenario-based-codebase/OOPsScenarios/src/com/swiftcart/PerishableProduct.java
@@ -0,0 +1,14 @@
+package com.swiftcart;
+
+public class PerishableProduct extends Product {
+
+    public PerishableProduct(String name, double price) {
+        super(name, price, "Perishable");
+    }
+
+    @Override
+    public double getDiscount() {
+        return price * 0.20; // 20% discount
+    }
+}
+
