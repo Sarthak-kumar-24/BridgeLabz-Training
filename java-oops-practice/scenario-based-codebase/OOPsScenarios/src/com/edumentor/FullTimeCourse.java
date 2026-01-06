bb33c943d03582fcbd45a3ed824a309b879fa675
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-06 20:31:10 +0530

    [Sarthak]: added Edu-Mentor program's FullTimeCourse class

---
+++ java-oops-practice/scenario-based-codebase/OOPsScenarios/src/com/edumentor/FullTimeCourse.java
@@ -0,0 +1,27 @@
+package com.edumentor;
+
+/*
+ *  EduMentor – Personalized Learning Platform
+ *  User base class: name, email, userId.
+ *  Learner and Instructor inherit from User.
+ *  ICertifiable interface with method generateCertificate().
+ *  Quiz class: fields for questions, answers, and score.
+ *  Encapsulation for quiz answers (can’t be modified once set).
+ *  Polymorphism: generateCertificate() works differently for short courses vsfull-time.
+ *  
+ * 
+ */
+public class FullTimeCourse implements ICertifiable  {
+	
+	private String courseName;
+	
+	public FullTimeCourse( String courseName) {
+		this.courseName = courseName;
+	}
+	
+	
+	public void generateCertificate() {
+		System.out.println("Certificate of Completion for Full Time Course");
+	}
+
+}
