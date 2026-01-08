bcb26184093e0ea30cc4dba32dfd587f62b17d08
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-08 22:01:54 +0530

    [Sarthak]: added MediStore program's Injection class

---
+++ java-oops-practice/scenario-based-codebase/OOPsScenarios/src/com/medistore/Injection.java
@@ -0,0 +1,34 @@
+package com.medistore;
+
+/* MediStore – Pharmacy Inventory & Sales System
+ * 
+ *  A pharmacy chain needs an app to manage drug inventory, sales, and expiry tracking.
+ *  
+ *  Medicine class: name, price, expiryDate, quantity.
+ *  Constructors for medicines with default quantity.
+ *  Use operators: total price, discount, stock adjustments.
+ *  Inheritance: Tablet, Syrup, Injection extend Medicine.
+ *  Polymorphism: checkExpiry() works differently for preserved vs liquid meds
+ *  
+ */
+public class Injection extends Medicine {
+
+	private String name;
+	private int quantity;
+	private boolean isExpired;
+
+	public Injection(String name, int price, int expiryDate, int quantity, boolean isExpired) {
+		super(name, price, expiryDate, quantity, isExpired);
+
+	}
+
+	@Override
+	public boolean checkExpiry() {
+		if (this.isExpired == true) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
