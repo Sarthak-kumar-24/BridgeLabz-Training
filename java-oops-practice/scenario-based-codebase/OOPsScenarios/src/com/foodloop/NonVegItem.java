248ff4d8da62202c45e21dfd47b9d0071a9d2126
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-09 20:58:20 +0530

    [Sarthak]: added Food-Loop program's NonVegItem

---
+++ java-oops-practice/scenario-based-codebase/OOPsScenarios/src/com/foodloop/NonVegItem.java
@@ -0,0 +1,13 @@
+package com.foodloop;
+
+public class NonVegItem extends FoodItem {
+
+    public NonVegItem(String name, double price, int stock) {
+        super(name, "Non-Veg", price, stock);
+    }
+
+    @Override
+    public String getType() {
+        return " Non-Veg";
+    }
+}
