1e17e109e557895fa797846a6757400e268d7339
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-13 20:48:54 +0530

    [Sarthak]: added Bird Sancturay program's Bird class

---
+++ java-oops-practice/scenario-based-codebase/OOPsScenarios/src/com/birdsanctuary/Bird.java
@@ -0,0 +1,34 @@
+package com.birdsanctuary;
+
+/* ======================================================
+ * 
+ * Acts as the parent class for all bird types
+ * Holds common properties like id, name, species
+ * Defines common behavior like eat()
+ * Enforces polymorphism via abstract showInfo()
+ * 
+ * ======================================================
+ */
+public abstract class Bird {
+	
+	   protected int id;
+	    protected String name;
+	    protected String species;
+
+	    public Bird(int id, String name, String species) {
+	        this.id = id;
+	        this.name = name;
+	        this.species = species;
+	    }
+
+	    public int getId() {
+	        return id;
+	    }
+
+	    public void eat() {
+	        System.out.println(name + " is eating.");
+	    }
+
+	    public abstract void showInfo();
+
+}
