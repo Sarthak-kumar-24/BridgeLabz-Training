1de17504fab6014b8c848d1458d1bbc6d4cabb0c
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-15 20:57:54 +0530

    [Sarthak]: added Dynamic Online MarketPlace System BookCategory program

---
+++ collection-practice/gcr-codebase/CollectionPractice/src/com/generics/dynamiconlinemarketplace/BookCategory.java
@@ -0,0 +1,11 @@
+package com.generics.dynamiconlinemarketplace;
+
+/**
+ * BookCategory represents the category for books.
+ */
+public class BookCategory implements Category {
+    public String getCategoryName() {
+        return "Books";
+    }
+
+}
