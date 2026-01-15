fb5df2acaecefbbcaaf255cb9f5ba8125c613fac
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-15 20:59:27 +0530

    [Sarthak]: added Dynamic Online MarketPlace System  ClothingCategory program

---
+++ collection-practice/gcr-codebase/CollectionPractice/src/com/generics/dynamiconlinemarketplace/ClothingCategory.java
@@ -0,0 +1,11 @@
+package com.generics.dynamiconlinemarketplace;
+
+/**
+ * ClothingCategory represents the category for clothing items.
+ */
+public class ClothingCategory implements Category {
+    public String getCategoryName() {
+        return "Clothing";
+    }
+
+}
