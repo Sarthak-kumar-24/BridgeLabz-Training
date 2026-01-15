a980a8bbbba5c6dc70f78548c54faef9c45b3f14
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-15 20:37:58 +0530

    [Sarthak]: added Exam Cell program

---
+++ dsa-practice/scenario-based-codebase/DSAScenarios/src/com/examcell/Student.java
@@ -0,0 +1,12 @@
+package com.examcell;
+
+public class Student {
+
+    int rollNo;
+    int score;
+
+    Student(int rollNo, int score) {
+        this.rollNo = rollNo;
+        this.score = score;
+    }
+}
