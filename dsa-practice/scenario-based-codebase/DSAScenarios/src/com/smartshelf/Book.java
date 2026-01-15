e84d75f3632a61a50e677cb78d8e0c02395b3cac
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-15 20:40:09 +0530

    [Sarthak]: added Smart Shelf Program's book class

---
+++ dsa-practice/scenario-based-codebase/DSAScenarios/src/com/smartshelf/Book.java
@@ -0,0 +1,14 @@
+package com.smartshelf;
+
+public class Book {
+	
+    int id;
+    String title;
+
+    
+    Book(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+}
