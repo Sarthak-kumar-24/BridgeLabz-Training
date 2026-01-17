819204da82e8d8871afb1b54e2e2e116d9cb11aa
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-17 21:29:48 +0530

    [Sarthak]: added Art Expo program's Artist class

---
+++ dsa-practice/scenario-based-codebase/DSAScenarios/src/com/artexpo/Artist.java
@@ -0,0 +1,13 @@
+package com.artexpo;
+
+public class Artist {
+	
+    String name;
+    int registrationTime; // minutes since opening
+
+    Artist(String name, int registrationTime) {
+        this.name = name;
+        this.registrationTime = registrationTime;
+    }
+
+}
