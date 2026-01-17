297516168006c686e2c6a6b4dab261937c380962
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-17 21:58:32 +0530

    [Sarthak]: added Bag and Ball Organizer program's Storable class

---
+++ java-oops-practice/scenario-based-codebase/OOPsScenarios/src/com/bagnballorganizer/Storable.java
@@ -0,0 +1,13 @@
+package com.bagnballorganizer;
+
+/*
+ * Storable Interface
+ * ------------------
+ * Defines a common behavior for all objects
+ * that can be stored in the system.
+ * Both Bag and Ball implement this interface.
+ */
+public interface Storable {
+	 void displayInfo();
+
+}
