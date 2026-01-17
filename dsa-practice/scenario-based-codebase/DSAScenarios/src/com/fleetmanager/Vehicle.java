a1f11ed1dc9e726457228edf88afe836faae6876
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-17 21:31:17 +0530

    [Sarthak]: added Fleetmanager program's Vehicle class

---
+++ dsa-practice/scenario-based-codebase/DSAScenarios/src/com/fleetmanager/Vehicle.java
@@ -0,0 +1,17 @@
+package com.fleetmanager;
+
+/*
+ * Vehicle class represents each vehicle in the fleet
+ * 
+ */
+public class Vehicle {
+	
+    int vehicleId;
+    int mileage;
+
+    Vehicle(int vehicleId, int mileage) {
+        this.vehicleId = vehicleId;
+        this.mileage = mileage;
+    }
+
+}
