ae4e351d2beed0bfcf5835ca95722db204d3797e
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-17 21:44:10 +0530

    [Sarthak]: added SmartLibrary program's Book class

---
+++ dsa-practice/scenario-based-codebase/DSAScenarios/src/com/smartlibrary/Book.java
@@ -0,0 +1,13 @@
+package com.smartlibrary;
+
+/*
+ * Book class represents a borrowed book
+ */
+public class Book {
+    String title;
+
+    Book(String title) {
+        this.title = title;
+    }
+
+}
