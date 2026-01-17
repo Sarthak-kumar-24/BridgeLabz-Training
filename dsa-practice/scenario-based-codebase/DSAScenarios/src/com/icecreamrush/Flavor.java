8cd9b233ac058c6e843129ad0d509e6c84ba3755
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-17 21:42:34 +0530

    [Sarthak]: added Ice cream Rush program's Flavor class

---
+++ dsa-practice/scenario-based-codebase/DSAScenarios/src/com/icecreamrush/Flavor.java
@@ -0,0 +1,15 @@
+package com.icecreamrush;
+
+/*
+ * Flavor class represents each ice cream flavor
+ */
+public class Flavor {
+    String name;
+    int weeklySales;
+
+    Flavor(String name, int weeklySales) {
+        this.name = name;
+        this.weeklySales = weeklySales;
+    }
+
+}
