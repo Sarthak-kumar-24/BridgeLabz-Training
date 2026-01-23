c1cb9659be4a1ac756c92d2b3f372e536db22abd
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-23 20:22:28 +0530

    [Sarthak]: addd Resume Analyzer program's ResumeException class

---
+++ collection-practice/scenario-based-codebase/CollectionScenarios/src/com/resumeanalyze/ResumeException.java
@@ -0,0 +1,9 @@
+package com.resumeanalyze;
+
+//Thrown when resume format is invalid
+public class ResumeException extends Exception {
+ public ResumeException(String message) {
+     super(message);
+ }
+}
+
