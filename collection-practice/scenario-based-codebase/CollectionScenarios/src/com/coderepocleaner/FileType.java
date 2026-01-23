1ac7bf4b4889d8ebc63d999769087ea32b711a16
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-23 20:20:40 +0530

    [Sarthak]: addd code repo cleaner program's FileType class

---
+++ collection-practice/scenario-based-codebase/CollectionScenarios/src/com/coderepocleaner/FileType.java
@@ -0,0 +1,11 @@
+package com.coderepocleaner;
+
+/**
+ * FileType --------- Represents classification category for Java source files
+ * after analysis.
+ */
+public enum FileType {
+
+	VALID, WARNINGS, ERRORS
+
+}
