dd784a57e8d5dc4d13d571640e8058c27f604855
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-24 21:28:58 +0530

    [Sarthak]: added Testing Parameterized Tests program NumberUtils class

---
+++ collection-practice/gcr-codebase/CollectionPractice/src/com/junit/testingparameterizedtests/NumberUtils.java
@@ -0,0 +1,14 @@
+package com.junit.testingparameterizedtests;
+
+/*
+ * NumberUtils
+ * -----------
+ * Utility class for number-related operations.
+ */
+public class NumberUtils {
+
+    // Returns true if number is even
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
