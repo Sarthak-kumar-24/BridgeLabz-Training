bf269f9c617692210e02f3f19831d251a8a62654
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-24 21:21:42 +0530

    [Sarthak]: added Performance Testing Using @Timeout program

---
+++ collection-practice/gcr-codebase/CollectionPractice/src/com/junit/performancetestingusingTimeout/PerformanceUtilsTest.java
@@ -0,0 +1,25 @@
+package com.junit.performancetestingusingTimeout;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+/*
+ * PerformanceUtilsTest
+ * --------------------
+ * Demonstrates performance testing using @Timeout.
+ */
+public class PerformanceUtilsTest {
+
+    PerformanceUtils utils = new PerformanceUtils();
+
+    /* ===== PERFORMANCE TEST ===== */
+    @Test
+    @Timeout(2) // fails if execution takes more than 2 seconds
+    void testLongRunningTaskPerformance() {
+
+        String result = utils.longRunningTask();
+        assertEquals("Task completed", result);
+    }
+}
+
