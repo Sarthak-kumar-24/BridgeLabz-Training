a250c671aa63662984e5dc69c5d594d52adfae4e
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-27 14:24:46 +0530

    [Sarthak]: moved NumberUtilsTest class to test folder

--- collection-practice/gcr-codebase/CollectionPractice/src/com/junit/test/NumberUtilsTest.java
+++ collection-practice/gcr-codebase/CollectionPractice/src/com/junit/test/NumberUtilsTest.java
@@ -1,8 +1,10 @@
-package com.junit.testingparameterizedtests;
+package com.junit.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import com.junit.main.NumberUtils;
 
 /*
  * NumberUtilsTest
