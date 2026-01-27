891dfecd1ba51eecf633ff138976f78a7a2cc638
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-27 14:22:52 +0530

    [Sarthak]: moved TemperatureConverterTest class to test folder

--- collection-practice/gcr-codebase/CollectionPractice/src/com/junit/test/TemperatureConverterTest.java
+++ collection-practice/gcr-codebase/CollectionPractice/src/com/junit/test/TemperatureConverterTest.java
@@ -1,7 +1,9 @@
-package com.junit.testingtemperatureconverter;
+package com.junit.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+
+import com.junit.main.TemperatureConverter;
 
 /*
  * TemperatureConverterTest
