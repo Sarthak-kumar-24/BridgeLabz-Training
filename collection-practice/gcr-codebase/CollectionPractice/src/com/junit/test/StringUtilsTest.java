9f57e88186512a44812f4db9eeca9bbcb8b7e03c
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-27 14:23:21 +0530

    [Sarthak]: moved StringUtilsTest class to test folder

--- collection-practice/gcr-codebase/CollectionPractice/src/com/junit/test/StringUtilsTest.java
+++ collection-practice/gcr-codebase/CollectionPractice/src/com/junit/test/StringUtilsTest.java
@@ -1,7 +1,9 @@
-package com.junit.testingstringutilitymethods;
+package com.junit.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+
+import com.junit.main.StringUtils;
 
 /*
  * StringUtilsTest
