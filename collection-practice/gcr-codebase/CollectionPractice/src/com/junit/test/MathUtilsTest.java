61e31a06c1f8c6166e4b0e3623236722f888c9b6
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-27 14:25:17 +0530

    [Sarthak]: moved MathUtilsTest class to test folder

--- collection-practice/gcr-codebase/CollectionPractice/src/com/junit/test/MathUtilsTest.java
+++ collection-practice/gcr-codebase/CollectionPractice/src/com/junit/test/MathUtilsTest.java
@@ -1,7 +1,9 @@
-package com.junit.testingexceptionhandling;
+package com.junit.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+
+import com.junit.main.MathUtils;
 
 /*
  * MathUtilsTest
