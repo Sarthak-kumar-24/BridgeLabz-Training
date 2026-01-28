016834e2c030e465695df6d0df530d38f3b32fee
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-01-28 22:24:46 +0530

    [Sarthak]: added Backupable class

---
+++ java8-practice/gcr-codebase/Java8Practice/src/com/functionalinterfaces/dataserializationforbackup/Backupable.java
@@ -0,0 +1,11 @@
+package com.functionalinterfaces.dataserializationforbackup;
+
+/*
+ * Backupable Marker Interface
+ * ---------------------------
+ * Marks classes that are eligible
+ * for backup processing.
+ */
+public interface Backupable {
+	// No methods (marker interface)
+}
